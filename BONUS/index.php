3a0ba0b20d9af3fd564b51f701f05c35f28ed3e4
Quintusdev <quintusdev92@gmail.com>
2023-07-10 10:37:24 +0200

    BONUS 1 COMPLETATO

--- BONUS/index.php
+++ BONUS/index.php
@@ -1,79 +1,101 @@
 <?php
-    // Definizione della classe Movie
     class Movie {
-        // Proprietà della classe Movie
         public $titolo;
-        public $genere;
+        public $generi;
         public $anno;
         public $voto;
         public $prezzo_biglietto;
 
-        // Costruttore della classe Movie
-        function __construct($titolo, $genere, $anno, $voto, $prezzo_biglietto){
-            // Inizializzazione delle proprietà dell'oggetto
+        function __construct($titolo, $generi, $anno, $voto, $prezzo_biglietto){
             $this->titolo = $titolo;
-            $this->genere = $genere;
+            $this->generi = $generi;
             $this->anno = $anno;
             $this->voto = $voto;
             $this->prezzo_biglietto = $prezzo_biglietto;
         }
 
-        // Metodo per impostare il prezzo finale del biglietto
         function setPrezzo($prezzo_biglietto){
-            // Verifica se l'anno di uscita è precedente al 2015
             if($this->anno < 2015) {
-                // Applica uno sconto di 10 al prezzo del biglietto
-                $this->prezzo_biglietto =$prezzo_biglietto - 10;
+                $this->prezzo_biglietto = $prezzo_biglietto - 10;
             } else {
-                // L'anno di uscita è successivo o uguale al 2015, il prezzo rimane invariato
-                $this->prezzo_biglietto =$prezzo_biglietto;
+                $this->prezzo_biglietto = $prezzo_biglietto;
             }
         }
-        // Metodo per ottenere il prezzo finale del biglietto
+
         function getPrezzo(){
             return $this->prezzo_biglietto;
         }
-
     }
-    // Creazione degli oggetti Movie
-        /* FILM 1 */
-            $movie1 = new Movie('Ritorno al futuro', 'Fantascienza', 1984, '9', 20);
-            $movie1->setPrezzo($movie1->prezzo_biglietto);
-        /* FILM2 */
-            $movie2 = new Movie('La mummia', 'Horror', 2006, '7', 35);
-            $movie2->setPrezzo($movie2->prezzo_biglietto);
-        /* FILM 3 */
-            $movie3 = new Movie('LOL', 'Comico', 2022, '8', 45);
-            $movie3->setPrezzo($movie3->prezzo_biglietto);
-        /* FILM 4 */
-            $movie4 = new Movie('Boolean il film', 'Fantascienza', 2023, '10', 100);
-            $movie4->setPrezzo($movie4->prezzo_biglietto);
 
-    // Visualizzazione delle informazioni sui film
+    class Genere {
+        public $nome;
+
+        function __construct($nome) {
+            $this->nome = $nome;
+        }
+    }
+
+    $generi = [
+        new Genere('Fantascienza'),
+        new Genere('Horror'),
+        new Genere('Comico'),
+        new Genere('Azione')
+    ];
+
+    $movie1 = new Movie('Ritorno al futuro', $generi, 1984, '9', 20);
+    $movie1->setPrezzo($movie1->prezzo_biglietto);
+
+    $movie2 = new Movie('La mummia', $generi, 2006, '7', 35);
+    $movie2->setPrezzo($movie2->prezzo_biglietto);
+
+    $movie3 = new Movie('LOL', $generi, 2022, '8', 45);
+    $movie3->setPrezzo($movie3->prezzo_biglietto);
+
+    $movie4 = new Movie('Boolean il film', $generi, 2023, '10', 100);
+
+    $movie4->setPrezzo($movie4->prezzo_biglietto);
+
     echo "<pre>";
     echo "<hr/>";
-    echo "<h1><strong>"."Primo Film:"."</strong></h1>";
-    echo "<h2><strong>Genere: ".$movie1->genere."</strong></h2>";
+    echo "<h1><strong>Primo Film:</strong></h1>";
+    echo "<h2><strong>Genere: ";
+    foreach ($movie1->generi as $genere) {
+        echo $genere->nome . " ";
+    }
+    echo "</strong></h2>";
     echo "<h2><strong>Anno di uscita: ".$movie1->anno."</strong></h2>";
-    echo "<h2><strong>Voto: ".$movie1->voto."</strong></h42";
-    echo "<h2><strong>Prezzo Biglietto: ".$movie1->prezzo_biglietto."€"."</strong></h2>";
+    echo "<h2><strong>Voto: ".$movie1->voto."</strong></h2>";
+    echo "<h2><strong>Prezzo Biglietto: ".$movie1->prezzo_biglietto."€</strong></h2>";
     echo "<hr/>";
-    echo "<h1><strong>"."Secondo Film:"."</strong></h1>";
-    echo "<h2><strong>Genere: ".$movie2->genere."</strong></h2>";
+    echo "<h1><strong>Secondo Film:</strong></h1>";
+    echo "<h2><strong>Genere: ";
+    foreach ($movie2->generi as $genere) {
+        echo $genere->nome . " ";
+    }
+    echo "</strong></h2>";
     echo "<h2><strong>Anno di uscita: ".$movie2->anno."</strong></h2>";
     echo "<h2><strong>Voto: ".$movie2->voto."</strong></h2>";
-    echo "<h2><strong>Prezzo Biglietto: ".$movie2->prezzo_biglietto."€"."</strong></h2>";
+    echo "<h2><strong>Prezzo Biglietto: ".$movie2->prezzo_biglietto."€</strong></h2>";
     echo "<hr/>";
-    echo "<h1><strong>"."Terzo Film:"."</strong></h1>";
-    echo "<h2><strong>Genere: ".$movie3->genere."</strong></h2>";
+    echo "<h1><strong>Terzo Film:</strong></h1>";
+    echo "<h2><strong>Genere: ";
+    foreach ($movie3->generi as $genere) {
+        echo $genere->nome . " ";
+    }
+    echo "</strong></h2>";
     echo "<h2><strong>Anno di uscita: ".$movie3->anno."</strong></h2>";
     echo "<h2><strong>Voto: ".$movie3->voto."</strong></h2>";
-    echo "<h2><strong>Prezzo Biglietto: ".$movie3->prezzo_biglietto."€"."</strong></h2>";
+    echo "<h2><strong>Prezzo Biglietto: ".$movie3->prezzo_biglietto."€</strong></h2>";
     echo "<hr/>";
-    echo "<h1><strong>"."Quarto Film:"."</strong></h1>";
-    echo "<h2><strong>Genere: ".$movie4->genere."</strong></h2>";
+    echo "<h1><strong>Quarto Film:</strong></h1>";
+    echo "<h2><strong>Genere: ";
+    foreach ($movie4->generi as $genere) {
+        echo $genere->nome . " ";
+    }
+    echo "</strong></h2>";
     echo "<h2><strong>Anno di uscita: ".$movie4->anno."</strong></h2>";
     echo "<h2><strong>Voto: ".$movie4->voto."</strong></h2>";
-    echo "<h2><strong>Prezzo Biglietto: ".$movie4->prezzo_biglietto."€"."</strong></h2>";
+    echo "<h2><strong>Prezzo Biglietto: ".$movie4->prezzo_biglietto."€</strong></h2>";
     echo "<hr/>";
+    echo "</pre>";
 ?>
